8d0bfbf6c000d57c1523e1d565c6639769e60ebb
Bogdan <vlasenko_b@itstep.academy>
2021-02-16 15:37:50 +0300

    SSL enabled

--- src/main/java/khpi/khpi_olympiad/KhpiOlympiadApplication.java
+++ src/main/java/khpi/khpi_olympiad/KhpiOlympiadApplication.java
@@ -1,7 +1,14 @@
 package khpi.khpi_olympiad;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KhpiOlympiadApplication {
@@ -10,4 +17,30 @@ public class KhpiOlympiadApplication {
         SpringApplication.run(KhpiOlympiadApplication.class, args);
     }
 
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomkat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint constraint = new SecurityConstraint();
+                constraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+            }
+        };
+        tomkat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+        return tomkat;
+    }
+
+    public Connector httpToHttpsRedirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
 }
