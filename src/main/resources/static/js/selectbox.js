5d82dfb1f259f88a33ad34b8afe3a1e3418ad6ee
Bogdan <vlasenko_b@itstep.academy>
2021-03-21 17:44:05 +0300

    add multiple selectbox

--- src/main/resources/static/js/selectbox.js
+++ src/main/resources/static/js/selectbox.js
@@ -1,37 +1,54 @@
-const selected = document.querySelector(".selected")
-const optionsContainer = document.querySelector(".options-container")
-const optionsList = document.querySelectorAll(".option")
-const searchBox = document.querySelector(".search-box input")
+const selectedAll = document.querySelectorAll(".selected")
 
 
-selected.addEventListener("click", () => {
-        optionsContainer.classList.toggle("active")
-        searchBox.value = ""
-        filterList("")
-        if (optionsContainer.classList.contains("active")) {
-            searchBox.focus()
+selectedAll.forEach(selected => {
+
+    const optionsContainer = selected.previousElementSibling
+    const searchBox = selected.nextElementSibling
+
+    const optionsList = optionsContainer.querySelectorAll(".option")
+
+    selected.addEventListener("click", () => {
+            if (optionsContainer.classList.contains("active")) {
+                optionsContainer.classList.remove("active")
+            } else {
+                let currentActive = document.querySelector(".options-container.active")
+                if (currentActive) {
+                    currentActive.classList.remove("active")
+                }
+                optionsContainer.classList.add("active")
+            }
+
+            searchBox.value = ""
+            filterList("")
+            if (optionsContainer.classList.contains("active")) {
+                searchBox.focus()
+            }
         }
+    )
+
+    optionsList.forEach(o => {
+        o.addEventListener("click", () => {
+            selected.innerHTML = o.querySelector("label").innerHTML
+            document.getElementById("universityName").value = selected.innerHTML
+            optionsContainer.classList.remove("active")
+        })
+    })
+    searchBox.addEventListener("keyup", function (e) {
+        filterList(e.target.value)
+    })
+
+    const filterList = searchTerm => {
+        searchTerm = searchTerm.toLowerCase()
+        optionsList.forEach(option => {
+            let label = option.firstElementChild.nextElementSibling.innerText.toLowerCase()
+            if (label.indexOf(searchTerm) != -1) {
+                option.style.display = "block"
+            } else {
+                option.style.display = "none"
+            }
+        })
     }
-)
-optionsList.forEach(o => {
-    o.addEventListener("click", () => {
-        selected.innerHTML = o.querySelector("label").innerHTML
-        document.getElementById("universityName").value = selected.innerHTML
-        optionsContainer.classList.remove("active")
-    })
-})
-searchBox.addEventListener("keyup", function (e) {
-    filterList(e.target.value)
+
 })
 
-const filterList = searchTerm => {
-    searchTerm = searchTerm.toLowerCase()
-    optionsList.forEach(option => {
-        let label = option.firstElementChild.nextElementSibling.innerText.toLowerCase()
-        if (label.indexOf(searchTerm) != -1) {
-            option.style.display = "block"
-        } else {
-            option.style.display = "none"
-        }
-    })
-}
